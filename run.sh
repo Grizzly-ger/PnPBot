b567c30f4b406fa769577798fe9e412b8dc46f25
Grizzly90 <62837071+Grizzly90@users.noreply.github.com>
2022-01-21 16:40:45 +0100

    small tests

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ screen -rd bot -X stuff "^C"
 sleep 5
 screen -rd bot -X stuff $"exit\n"
 screen -dmS bot
-screen -S bot -X stuff $"source bot-venv/bin/activate\n"
+screen -S bot -X stuff $"source PnPBot/bot-venv/bin/activate\n"
 screen -S bot -X stuff $"pip3 install -U nextcord[voice] python-dotenv Beautifulsoup4 urllib3 tabulate\n"
 screen -S bot -X stuff $"python3 PnPBot/bot.py\n"
 
@@ -11,5 +11,5 @@ screen -rd foundrysocket -X stuff "^C"
 sleep 5
 screen -rd foundrysocket -X stuff $"exit\n"
 screen -dmS foundrysocket
-screen -S foundrysocket -X stuff $"source bot-venv/bin/activate\n"
+screen -S foundrysocket -X stuff $"source PnPBot/bot-venv/bin/activate\n"
 screen -S foundrysocket -X stuff $"python3 PnPBot/foundrysocket.py -s\n"
